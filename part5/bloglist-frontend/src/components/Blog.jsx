29f0a42e1a8a07e60524cde379ce03e552074dc4
OviniP <149691951+OviniP@users.noreply.github.com>
2024-03-16 11:02:52 +0200

    5.7 done

--- part5/bloglist-frontend/src/components/Blog.jsx
+++ part5/bloglist-frontend/src/components/Blog.jsx
@@ -1,7 +1,40 @@
-const Blog = ({ blog }) => (
-  <div>
-    {blog.title} {blog.author}
-  </div>  
-)
+import { useState } from "react"
+
+const Blog = ({ blog }) => {
+
+  const [isExpanded, setIsExpanded] = useState(false)
+  const [expandLabel, setExpandLabel] = useState('View')
+  
+  const blogStyle = {
+    paddingTop: 10,
+    paddingLeft: 2,
+    border: 'solid',
+    borderWidth: 1,
+    marginBottom: 5
+  }
+
+  const changeExpandability = () => {
+    setIsExpanded(!isExpanded)
+    const lablelText = isExpanded ?  'View' : 'Hide'
+    setExpandLabel(lablelText)
+  }
+
+  return (
+  <div style={blogStyle}>
+    <div>
+      <div>
+        {blog.title}
+        <button onClick={changeExpandability}>{expandLabel}</button>
+      </div>
+      {isExpanded && 
+        <div>
+        {blog.url}<br/>
+        {blog.likes}<br/>
+        {blog.author}<br/>
+        </div>
+      }
+    </div> 
+  </div> 
+)}
 
 export default Blog
